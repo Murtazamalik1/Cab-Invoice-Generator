fe915f592127c384ed67e5ba58a19683a56e2497
murtazamalik <murtazamalik1319@gmail.com>
2023-04-09 22:46:46 +0530

    Added: cabInVoice Generator

---
+++ src/cab/InvoiceService.java
@@ -0,0 +1,13 @@
+package cab;
+
+public class InvoiceService {
+    static Invoice generateInvoiceById(int userId) {
+
+        for (Customer customer : RideRepository.customerList) {
+            if (customer.getId() == userId) {
+                return InvoiceGenerator.generateInvoice(customer.getRides());
+            }
+        }
+        return null;
+    }
+}
---
+++ src/cab/RideRepository.java
@@ -0,0 +1,8 @@
+package cab;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RideRepository {
+    static List<Customer> customerList=new ArrayList<>();
+}
